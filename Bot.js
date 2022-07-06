9d6bf7106f20e946b9536916db5edc315d4d74d8
morfua <69007202+morfua@users.noreply.github.com>
2022-07-06 16:06:12 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  For google
-// @author       Sergey Chizhikov
+// @author       Gordeev Sergey
 // @match        https://www.google.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
